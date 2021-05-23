f974c2a5090c97224eaf4ae6d16be283a05fd7a2
Janis Feldmanis <feldmanis45@gmail.com>
2021-05-23 14:05:42 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,117 @@
+window.onload=function() {
+    canv=document.getElementById("gc");
+    ctx=canv.getContext("2d");
+    document.addEventListener("keydown",keyPush);
+    window.addEventListener('gamepadconnected', (event) => {
+        gamepadIndex = event.gamepad.index;
+        gamepad_action(gamepadIndex);
+    });
+    setInterval(game,1000/15);
+}
+px=py=10;
+gs=tc=20;
+ax=ay=15;
+xv=yv=0;
+trail=[];
+tail = 5;
+function game() {
+    px+=xv;
+    py+=yv;
+    if(px<0) {
+        px= tc-1;
+    }
+    if(px>tc-1) {
+        px= 0;
+    }
+    if(py<0) {
+        py= tc-1;
+    }
+    if(py>tc-1) {
+        py= 0;
+    }
+    ctx.fillStyle="black";
+    ctx.fillRect(0,0,canv.width,canv.height);
+ 
+    ctx.fillStyle="lime";
+    for(var i=0;i<trail.length;i++) {
+        ctx.fillRect(trail[i].x*gs,trail[i].y*gs,gs-2,gs-2);
+        if(trail[i].x==px && trail[i].y==py) {
+            document.getElementById("score").innerText="Spēle beigusies tev ir " + tail + " punkti";
+            tail = 5;
+            xv=yv=0;
+
+        }
+    }
+    trail.push({x:px,y:py});
+    while(trail.length>tail) {
+    trail.shift();
+    }
+ 
+    if(ax==px && ay==py) {
+        tail++;
+        document.getElementById("score").innerText="Rezultāts astei " + tail;
+        ax=Math.floor(Math.random()*tc);
+        ay=Math.floor(Math.random()*tc);
+    }
+    ctx.fillStyle="red";
+    ctx.fillRect(ax*gs,ay*gs,gs-2,gs-2);
+    
+}
+function keyPush(evt) {
+ 
+
+    switch(evt.keyCode) {
+        
+        case 37:
+            xv=-1;yv=0;
+            document.getElementById("score").innerText="Rezultāts astei " + tail;
+            break;
+        case 38:
+            xv=0;yv=-1;
+            document.getElementById("score").innerText="Rezultāts astei " + tail;
+            break;
+        case 39:
+            xv=1;yv=0;
+            document.getElementById("score").innerText="Rezultāts astei " + tail;
+            break;
+        case 40:
+            xv=0;yv=1;
+            document.getElementById("score").innerText="Rezultāts astei " + tail;
+            break;
+    }
+}
+function gamepad_action(gamepadIndex){
+ 
+    
+    // now print the axes on the connected gamepad, for example: 
+    setInterval(() => {
+        if(gamepadIndex !== undefined) {
+            // a gamepad is connected and has an index
+            const myGamepad = navigator.getGamepads()[gamepadIndex];
+    
+            console.log(`Left stick at (${myGamepad.axes[0]}, ${myGamepad.axes[1]})` );
+            
+            if(myGamepad.axes[0]<0.1){
+                xv=-1;yv=0;
+                document.getElementById("score").innerText="Rezultāts astei " + tail;
+                
+            }
+            if(myGamepad.axes[0]>-0.1){
+               
+                xv=1;yv=0;
+                document.getElementById("score").innerText="Rezultāts astei " + tail;
+            }
+            if(myGamepad.axes[1]>0.1){
+                
+                xv=0;yv=1;
+                document.getElementById("score").innerText="Rezultāts astei " + tail;
+            }
+            if(myGamepad.axes[1]<-0.1){
+               
+                xv=0;yv=-1;
+                document.getElementById("score").innerText="Rezultāts astei " + tail;
+            }
+    
+        }
+    }, 100) // print axes 10 times per second
+}
